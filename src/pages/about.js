47479ab51f2e72d968943f4f7c4e049f8b55739e
Burak Targaç <btargac@gmail.com>
2021-03-03 22:11:00 +0300

    add a common layout and use Link component

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>My name is Megan. I like teaching, video games, and dogs!</p>
+    </Layout>
+
+  )
+}
+
+export default AboutPage

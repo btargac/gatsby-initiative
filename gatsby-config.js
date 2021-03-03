c6f4db072dd4e15164cb6320ffc49ed90fc039bb
Burak Targaç <btargac@gmail.com>
2021-03-03 22:41:04 +0300

    add gatsby image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: "My Gatsby Initiative",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+  	`gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout'
 
@@ -6,6 +7,10 @@ import Layout from '../components/Layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+	  <StaticImage
+        alt="A reddish-brown pitbull looking longingly at the camera from behind a mason jar of overnight oats"
+        src="https://pbs.twimg.com/media/EsW978QUcAAj1Pe?format=jpg&name=4096x4096"
+      />
       <p>Welcome to my blog!</p>
     </Layout>
 

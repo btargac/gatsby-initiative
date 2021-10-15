be02090b59b2580ef9498069c14dfada4ec92f2a
Burak Targaç <btargac@gmail.com>
2021-10-15 19:02:38 +0300

    remove image from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout'
 
@@ -7,10 +6,6 @@ import Layout from '../components/Layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-	  <StaticImage
-        alt="Burak Targac"
-        src="https://media-exp1.licdn.com/dms/image/C4D03AQEo454zgxyeDA/profile-displayphoto-shrink_800_800/0/1610716455484?e=1639612800&v=beta&t=O4poNJzdhQ6M4ldomwoz8evJMVO2XdjKUZiiHirx3jI"
-      />
       <p>Welcome to my blog!</p>
     </Layout>
 

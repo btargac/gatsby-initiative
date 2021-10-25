4b03964838b1b824d79b7996126d26c130d391f1
Burak Targaç <btargac@gmail.com>
2021-10-25 18:20:50 +0300

    add gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-  	description: "The long awaited blog from Targac",
+  	description: "Things I've learned along the way, can help your journey too.",
     title: "Targac's Blog",
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
   	`gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
